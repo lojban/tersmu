87f2065a13e3ecfd92cc292eb6bf40357bbdf58a
Ludvig Böklin <ludvig.boklin@protonmail.com>
2020-08-20 20:07:29 +0200

    fix flake

--- flake.nix
+++ flake.nix
@@ -9,11 +9,13 @@
       pappy = pkgs.callPackage ./nix/pappy.nix {};
       src-gen = pkgs.callPackage ./nix/src-gen.nix { inherit pappy; };
       tersmu = pkgs.haskellPackages.callPackage ./default.nix { inherit src-gen; };
+      pappyApp = { type = "app"; program = "${pappy}/bin/pappy"; };
+      tersmuApp = { type = "app"; program = "${tersmu}/bin/tersmu"; };
 
     in {
       packages.x86_64-linux = { inherit pappy tersmu; };
       defaultPackage.x86_64-linux = tersmu;
-      apps.x86_64-linux = { inherit pappy tersmu; };
-      app.x86_64-linux = tersmu;
+      apps.x86_64-linux = { inherit pappyApp tersmuApp; };
+      defaultApp.x86_64-linux = tersmuApp;
     };
 }
